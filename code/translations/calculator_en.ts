7ccdd6dc0d830915b0274640783a5f996c4a647c
rylTianChen <lyrTianChen@outlook.com>
2026-07-08 22:05:47 +0800

    Add files via upload

---
+++ code/translations/calculator_en.ts
@@ -0,0 +1,283 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>MainWindow</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="107"/>
+        <location filename="../mainwindow.cpp" line="1065"/>
+        <source>高精度多功能计算器</source>
+        <translation>High-precision Multi-function Calculator</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="120"/>
+        <location filename="../mainwindow.cpp" line="1068"/>
+        <source>标准模式</source>
+        <translation>Standard</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="121"/>
+        <location filename="../mainwindow.cpp" line="1069"/>
+        <source>高精度模式</source>
+        <translation>High-precision</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="122"/>
+        <location filename="../mainwindow.cpp" line="1070"/>
+        <source>数论工具</source>
+        <translation>Number Theory</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="247"/>
+        <source>支持小数，运算范围：-2e9 ~ +2e9，适合日常使用</source>
+        <translation>Floating numbers supported, range: -2e9~+2e9, suitable for daily life</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="256"/>
+        <source>输入表达式，例如: 123 + 456*789</source>
+        <translation>Enter an expression, e.g.: 123 + 456*789</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="265"/>
+        <source>计算结果</source>
+        <translation>Result</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="270"/>
+        <location filename="../mainwindow.cpp" line="373"/>
+        <location filename="../mainwindow.cpp" line="489"/>
+        <location filename="../mainwindow.cpp" line="520"/>
+        <location filename="../mainwindow.cpp" line="559"/>
+        <location filename="../mainwindow.cpp" line="598"/>
+        <source>复制</source>
+        <translation>Copy</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="350"/>
+        <source>支持超大整数精确运算、部分位运算，但不支持小数</source>
+        <translation>Big integer calculation supported, but floating numbers are not supported</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="359"/>
+        <source>输入表达式，例如: 12345678901234567890 + 98765432109876543210</source>
+        <translation>Enter an expression, e.g.: 12345678901234567890 + 98765432109876543210</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="368"/>
+        <source>计算结果会显示在这里</source>
+        <translation>Result</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="455"/>
+        <source>一些简单数论功能，要求输入全部是正整数</source>
+        <translation>Some simple number theory functions, only positive integers are supported</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="462"/>
+        <source>根号化简</source>
+        <translation>Simplify Square Root</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="468"/>
+        <source>根号内数字</source>
+        <translation>Number under root</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="475"/>
+        <location filename="../mainwindow.cpp" line="505"/>
+        <location filename="../mainwindow.cpp" line="540"/>
+        <location filename="../mainwindow.cpp" line="579"/>
+        <source>计算</source>
+        <translation>Calculate</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="484"/>
+        <source>化简结果</source>
+        <translation>Result</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="498"/>
+        <source>分解质因数</source>
+        <translation>Prime Factorization</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="501"/>
+        <location filename="../mainwindow.cpp" line="853"/>
+        <location filename="../mainwindow.cpp" line="877"/>
+        <source>请输入一个数</source>
+        <translation>Please enter a number</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="515"/>
+        <source>分解结果</source>
+        <translation>Result</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="529"/>
+        <location filename="../mainwindow.cpp" line="554"/>
+        <source>最大公因数</source>
+        <translation>Greatest Common Divisor</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="532"/>
+        <location filename="../mainwindow.cpp" line="571"/>
+        <source>数A</source>
+        <translation>Number A</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="537"/>
+        <location filename="../mainwindow.cpp" line="576"/>
+        <source>数B</source>
+        <translation>Number B</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="568"/>
+        <location filename="../mainwindow.cpp" line="593"/>
+        <source>最小公倍数</source>
+        <translation>Least Common Multiple</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="633"/>
+        <location filename="../mainwindow.cpp" line="1073"/>
+        <source>设置</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="641"/>
+        <location filename="../mainwindow.cpp" line="1074"/>
+        <source>语言 / Language</source>
+        <translation>语言 / Language</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="645"/>
+        <location filename="../mainwindow.cpp" line="1075"/>
+        <source>语言：</source>
+        <translation>Language:</translation>
+    </message>
+    <message>
+        <source>这是设置</source>
+        <translation type="vanished">This is settings</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="806"/>
+        <location filename="../mainwindow.cpp" line="824"/>
+        <location filename="../mainwindow.cpp" line="849"/>
+        <location filename="../mainwindow.cpp" line="873"/>
+        <location filename="../mainwindow.cpp" line="899"/>
+        <location filename="../mainwindow.cpp" line="926"/>
+        <source>正在计算中...</source>
+        <translation>Calculating...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="902"/>
+        <location filename="../mainwindow.cpp" line="930"/>
+        <source>请输入两个数</source>
+        <translation>Please enter two numbers</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="956"/>
+        <location filename="../mainwindow.cpp" line="978"/>
+        <source>除零错误</source>
+        <translation>Division by zero</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="957"/>
+        <source>标准模式不支持取模运算(%)</source>
+        <translation>Modulo is not supported in Standard Mode</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="958"/>
+        <location filename="../mainwindow.cpp" line="980"/>
+        <source>输入格式错误</source>
+        <translation>Invalid input format</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="959"/>
+        <source>数字包含多个小数点</source>
+        <translation>Multiple dots in a single number</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="960"/>
+        <location filename="../mainwindow.cpp" line="981"/>
+        <source>括号不匹配：缺少右括号</source>
+        <translation>Mismatched parentheses: missing &apos;)&apos;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="961"/>
+        <location filename="../mainwindow.cpp" line="982"/>
+        <source>括号不匹配：缺少左括号</source>
+        <translation>Mismatched parentheses: missing &apos;(&apos;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="962"/>
+        <location filename="../mainwindow.cpp" line="983"/>
+        <source>包含非法字符</source>
+        <translation>Invalid character</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="963"/>
+        <location filename="../mainwindow.cpp" line="984"/>
+        <source>表达式无效</source>
+        <translation>Invalid expression</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="964"/>
+        <location filename="../mainwindow.cpp" line="985"/>
+        <source>不支持负次幂</source>
+        <translation>Negative power not supported</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="965"/>
+        <location filename="../mainwindow.cpp" line="986"/>
+        <source>0的0次幂无意义</source>
+        <translation>0^0 is undefined</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="966"/>
+        <location filename="../mainwindow.cpp" line="988"/>
+        <source>计算错误</source>
+        <translation>Calculation error</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="967"/>
+        <location filename="../mainwindow.cpp" line="989"/>
+        <location filename="../mainwindow.cpp" line="1000"/>
+        <location filename="../mainwindow.cpp" line="1009"/>
+        <location filename="../mainwindow.cpp" line="1018"/>
+        <location filename="../mainwindow.cpp" line="1027"/>
+        <source>未知错误</source>
+        <translation>Unknown error</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="979"/>
+        <source>模零错误</source>
+        <translation>Modulo by zero</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="987"/>
+        <source>幂运算数据过大</source>
+        <translation>Power operation result too large</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="998"/>
+        <location filename="../mainwindow.cpp" line="1007"/>
+        <location filename="../mainwindow.cpp" line="1016"/>
+        <location filename="../mainwindow.cpp" line="1025"/>
+        <source>输入不是正整数</source>
+        <translation>Input is not a positive integer</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="999"/>
+        <location filename="../mainwindow.cpp" line="1008"/>
+        <location filename="../mainwindow.cpp" line="1017"/>
+        <location filename="../mainwindow.cpp" line="1026"/>
+        <source>数据过大</source>
+        <translation>Number too large</translation>
+    </message>
+</context>
+</TS>
---
+++ code/translations/calculator_zh_CN.ts
@@ -0,0 +1,279 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>MainWindow</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="107"/>
+        <location filename="../mainwindow.cpp" line="1065"/>
+        <source>高精度多功能计算器</source>
+        <translation>高精度多功能计算器</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="120"/>
+        <location filename="../mainwindow.cpp" line="1068"/>
+        <source>标准模式</source>
+        <translation>标准模式</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="121"/>
+        <location filename="../mainwindow.cpp" line="1069"/>
+        <source>高精度模式</source>
+        <translation>高精度模式</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="122"/>
+        <location filename="../mainwindow.cpp" line="1070"/>
+        <source>数论工具</source>
+        <translation>数论工具</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="247"/>
+        <source>支持小数，运算范围：-2e9 ~ +2e9，适合日常使用</source>
+        <translation>支持小数，运算范围：-2e9 ~ +2e9，适合日常使用</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="256"/>
+        <source>输入表达式，例如: 123 + 456*789</source>
+        <translation>输入表达式，例如: 123 + 456*789</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="265"/>
+        <source>计算结果</source>
+        <translation>计算结果</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="270"/>
+        <location filename="../mainwindow.cpp" line="373"/>
+        <location filename="../mainwindow.cpp" line="489"/>
+        <location filename="../mainwindow.cpp" line="520"/>
+        <location filename="../mainwindow.cpp" line="559"/>
+        <location filename="../mainwindow.cpp" line="598"/>
+        <source>复制</source>
+        <translation>复制</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="350"/>
+        <source>支持超大整数精确运算、部分位运算，但不支持小数</source>
+        <translation>支持超大整数精确运算、部分位运算，但不支持小数</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="359"/>
+        <source>输入表达式，例如: 12345678901234567890 + 98765432109876543210</source>
+        <translation>输入表达式，例如: 12345678901234567890 + 98765432109876543210</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="368"/>
+        <source>计算结果会显示在这里</source>
+        <translation>计算结果会显示在这里</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="455"/>
+        <source>一些简单数论功能，要求输入全部是正整数</source>
+        <translation>一些简单数论功能，要求输入全部是正整数</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="462"/>
+        <source>根号化简</source>
+        <translation>根号化简</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="468"/>
+        <source>根号内数字</source>
+        <translation>根号内数字</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="475"/>
+        <location filename="../mainwindow.cpp" line="505"/>
+        <location filename="../mainwindow.cpp" line="540"/>
+        <location filename="../mainwindow.cpp" line="579"/>
+        <source>计算</source>
+        <translation>计算</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="484"/>
+        <source>化简结果</source>
+        <translation>化简结果</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="498"/>
+        <source>分解质因数</source>
+        <translation>分解质因数</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="501"/>
+        <location filename="../mainwindow.cpp" line="853"/>
+        <location filename="../mainwindow.cpp" line="877"/>
+        <source>请输入一个数</source>
+        <translation>请输入一个数</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="515"/>
+        <source>分解结果</source>
+        <translation>分解结果</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="529"/>
+        <location filename="../mainwindow.cpp" line="554"/>
+        <source>最大公因数</source>
+        <translation>最大公因数</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="532"/>
+        <location filename="../mainwindow.cpp" line="571"/>
+        <source>数A</source>
+        <translation>数A</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="537"/>
+        <location filename="../mainwindow.cpp" line="576"/>
+        <source>数B</source>
+        <translation>数B</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="568"/>
+        <location filename="../mainwindow.cpp" line="593"/>
+        <source>最小公倍数</source>
+        <translation>最小公倍数</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="633"/>
+        <location filename="../mainwindow.cpp" line="1073"/>
+        <source>设置</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="641"/>
+        <location filename="../mainwindow.cpp" line="1074"/>
+        <source>语言 / Language</source>
+        <translation>语言 / Language</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="645"/>
+        <location filename="../mainwindow.cpp" line="1075"/>
+        <source>语言：</source>
+        <translation>语言：</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="806"/>
+        <location filename="../mainwindow.cpp" line="824"/>
+        <location filename="../mainwindow.cpp" line="849"/>
+        <location filename="../mainwindow.cpp" line="873"/>
+        <location filename="../mainwindow.cpp" line="899"/>
+        <location filename="../mainwindow.cpp" line="926"/>
+        <source>正在计算中...</source>
+        <translation>正在计算中...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="902"/>
+        <location filename="../mainwindow.cpp" line="930"/>
+        <source>请输入两个数</source>
+        <translation>请输入两个数</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="956"/>
+        <location filename="../mainwindow.cpp" line="978"/>
+        <source>除零错误</source>
+        <translation>除零错误</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="957"/>
+        <source>标准模式不支持取模运算(%)</source>
+        <translation>标准模式不支持取模运算(%)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="958"/>
+        <location filename="../mainwindow.cpp" line="980"/>
+        <source>输入格式错误</source>
+        <translation>输入格式错误</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="959"/>
+        <source>数字包含多个小数点</source>
+        <translation>数字包含多个小数点</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="960"/>
+        <location filename="../mainwindow.cpp" line="981"/>
+        <source>括号不匹配：缺少右括号</source>
+        <translation>括号不匹配：缺少右括号</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="961"/>
+        <location filename="../mainwindow.cpp" line="982"/>
+        <source>括号不匹配：缺少左括号</source>
+        <translation>括号不匹配：缺少左括号</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="962"/>
+        <location filename="../mainwindow.cpp" line="983"/>
+        <source>包含非法字符</source>
+        <translation>包含非法字符</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="963"/>
+        <location filename="../mainwindow.cpp" line="984"/>
+        <source>表达式无效</source>
+        <translation>表达式无效</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="964"/>
+        <location filename="../mainwindow.cpp" line="985"/>
+        <source>不支持负次幂</source>
+        <translation>不支持负次幂</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="965"/>
+        <location filename="../mainwindow.cpp" line="986"/>
+        <source>0的0次幂无意义</source>
+        <translation>0的0次幂无意义</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="966"/>
+        <location filename="../mainwindow.cpp" line="988"/>
+        <source>计算错误</source>
+        <translation>计算错误</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="967"/>
+        <location filename="../mainwindow.cpp" line="989"/>
+        <location filename="../mainwindow.cpp" line="1000"/>
+        <location filename="../mainwindow.cpp" line="1009"/>
+        <location filename="../mainwindow.cpp" line="1018"/>
+        <location filename="../mainwindow.cpp" line="1027"/>
+        <source>未知错误</source>
+        <translation>未知错误</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="979"/>
+        <source>模零错误</source>
+        <translation>模零错误</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="987"/>
+        <source>幂运算数据过大</source>
+        <translation>幂运算数据过大</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="998"/>
+        <location filename="../mainwindow.cpp" line="1007"/>
+        <location filename="../mainwindow.cpp" line="1016"/>
+        <location filename="../mainwindow.cpp" line="1025"/>
+        <source>输入不是正整数</source>
+        <translation>输入不是正整数</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="999"/>
+        <location filename="../mainwindow.cpp" line="1008"/>
+        <location filename="../mainwindow.cpp" line="1017"/>
+        <location filename="../mainwindow.cpp" line="1026"/>
+        <source>数据过大</source>
+        <translation>数据过大</translation>
+    </message>
+</context>
+</TS>
